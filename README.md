b6924c6d3edfdbdfd5b0c8f10f3a321f5211617f
Andrew Hayward <github@andrewhayward.net>
2013-09-19 18:14:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,99 @@
-aestimia-client
+Aestimia Client
 ===============
 
 An Aestimia API Client
+
+Usage
+-----
+
+    var aestimia = require('aestimia-client')({
+      // The Aestimia server URL (REQUIRED)
+      endpoint: 'http://example.org/aestimia',
+      // Aestimia authentication secret (REQUIRED)
+      secret: '<secret key>',
+
+      // Minimum word count required for valid application descriptions
+      minWords: 10,
+      // List of responses to use in sensitive-applicant situations
+      cannedResponses: [
+        'Response 1',
+        'Response 2',
+        // ...
+        'Response n'
+      ],
+      // Logging callback function
+      logger: function (data) { console.dir(data); }
+    });
+    
+    aestimia.submit(application, function(err, id) {
+      if (err) {
+        // do something with the error
+      }
+      
+      // do something with the application ID
+    });
+    
+    aestimia.update(application, function(err, status) {
+      if (err) {
+        // do something with the error
+      }
+      
+      // do something with application status
+      /*
+        {
+          review: <latest review>,
+          accepted: <boolean>
+        }
+      */
+    })
+
+Expectations
+------------
+
+The following 'interfaces' are expected when using an Aestimia client. All fields and methods are required, unless otherwise stated.
+
+**`Application`:**
+
+* `description`  
+  description of application (or full body, if a simple text-based application)
+* `submissionId`  
+  submission ID of application, if already submitted (as returned by a successful `submit`)
+* *`getApplicant`*  
+  returns an `<Applicant>` object, via callback *`(err, applicant)`*
+* *`getBadge`*  
+  returns the `<Badge>` object being applied for, via callback *`(err, badge)`*
+* *`getCallbackUrl`*  
+  returns a fully formed URL to be hit by Aestimia service when application status changes
+* *`getCriteriaUrl`*  
+  returns a fully formed URL pointing to the criteria of this application
+* *`getEvidence`*  
+  returns an array of `<Evidence>` objects, via callback *`(err, evidence)`*
+
+**`Applicant`:**
+
+* `email`  
+  the email address of the applicant *(not required)*
+* `sensitive`  
+  a boolean value indicating whether this a 'sensitive' applicant, e.g. a young learner. If this is `true`, canned responses will be displayed to the auditor, and any email address will be withheld.
+
+**`Badge`:**
+
+* `categories`  
+  an array of 'tags' *(not required)*
+* `description`  
+  a description of the badge  
+* `image`  
+  a fully formed URL pointing to the badge's image
+* `name`  
+  the name of the badge
+* `rubric`  
+  the badge's evaluation rubric *(not required)* - see [Badge API docs](https://github.com/mozilla/openbadger/blob/v2.0/docs/api.md#badge-types)
+
+**`Evidence`:**
+
+* `description`  
+  a description of the evidence *(not required)*
+* `mediaType`  
+  an identifier for the file type
+* *`getUrl`*  
+  returns a fully formed URL where the item can be found in its raw form
---
+++ index.js
@@ -0,0 +1,200 @@
+const Api = require('./api');
+const _ = require('underscore');
+
+const DEFAULT_MIN_WORDS = 0;
+const DEFAULT_CANNED_RESPONSES = [
+  'You did a great job!',
+  'You went above and beyond.',
+  'Keep up the good work!',
+  'Good job.',
+  'You met all the criteria needed to earn this badge.',
+  'Creative and thoughtful work.',
+  'Nice reflection of your work.',
+  'You didn\'t submit relevant evidence.',
+  'Your evidence did not properly reflect the criteria.',
+  'Good work! But you still have a few criteria to meet to earn this badge. Make sure you take a look at all the criteria before reapplying.'
+];
+const DEFAULT_RUBRIC = {
+  text: 'Has done some work',
+  required: true
+}
+
+exports = module.exports = function (config) {
+  // The Aestimia server URL
+  const ENDPOINT = config.endpoint;
+  // Aestimia authentication secret
+  const SECRET = config.secret;
+
+  // Minimum word count required for valid application descriptions
+  const MIN_WORDS = config.minWords || DEFAULT_MIN_WORDS;
+  // List of responses to use in sensitive-applicant situations
+  const CANNED_RESPONSES = config.cannedResponses || DEFAULT_CANNED_RESPONSES;
+  // Logging callback function
+  const LOGGER = config.logger || function (data) {};
+
+  if (!ENDPOINT)
+    throw new ReferenceError('endpoint is not defined');
+  if (!SECRET)
+    throw new ReferenceError('secret is not defined');
+  if (!_.isFunction(LOGGER))
+    throw new TypeError('logger is not of type `function`');
+
+  var aestimia = new Api(ENDPOINT, {
+
+    /**
+     * Submit an application to an Aestimia server
+     *
+     * @param {Application} application The <application> to be submitted
+     * @param {function} callback Callback function
+     */
+    submit: function (application, callback) {
+      var api = this;
+      var description = (application.description||'').trim().replace(/[^a-z0-9\s]/ig, '');
+      var wordcount = !!description ? description.split(/\s+/).length : 0;
+
+      application.getEvidence(function(err, evidence) {
+        if (err)
+          return callback(err);
+
+        if (!evidence)
+          evidence = [];
+
+        if (!_.isArray(evidence))
+          return callback(new TypeError('evidence not of type `array`'));
+          
+        if (!evidence.length && wordcount < MIN_WORDS)
+          return callback(new Error('Insufficient evidence for this application'));
+
+        application.getApplicant(function(err, applicant) {
+          if (err)
+            return callback(err);
+
+          application.getBadge(function(err, badge) {
+            if (err)
+              return callback(err);
+
+            var rubric = badge.rubric || [DEFAULT_RUBRIC];
+            if (_.isArray(rubric))
+              rubric = {items: rubric};
+
+            var submission = {
+              criteriaUrl: application.getCriteriaUrl(badge),
+              onChangeUrl: application.getCallbackUrl(badge),
+              achievement: {
+                name: badge.name,
+                description: badge.description,
+                imageUrl: badge.image
+              },
+              classifications: badge.categories || [],
+              evidence: [],
+              rubric: rubric
+            };
+
+            if (applicant.email)
+              submission.email = applicant.email;
+
+            if (applicant.sensitive)
+              submission.cannedResponses = CANNED_RESPONSES;
+
+            if (application.description) {
+              submission.evidence.push({
+                url: application.getUrl(badge),
+                mediaType: 'link',
+                reflection: application.description
+              });
+            }
+
+            evidence.forEach(function(item, index) {
+              var type = item.mediaType.split('/')[0];
+              if (type !== 'image') type = 'link';
+
+              submission.evidence.push({
+                url: item.getUrl(),
+                reflection: item.description,
+                mediaType: type
+              });
+
+              LOGGER(submission);
+
+              api.post('/submission', {json:submission}, function (err, rsp) {
+                if (err)
+                  return callback(err);
+
+                callback(null, (rsp||{}).id);
+              });
+            });
+          });
+        });
+      });
+    },
+
+    /**
+     * Retrieve latest information about an application from an Aestimia server
+     *
+     * @param {Application} application The <application> to be updated
+     * @param {function} callback Callback function
+     */
+    update: function (application, callback) {
+      var api = this;
+
+      var submissionId = application.submissionId;
+
+      if (!submissionId)
+        return callback(new Error('Application has not yet been submitted'));
+
+      this.get('/submissions/' + submissionId, function (err, submission) {
+        if (err)
+          return callback(err);
+
+        var rubrics = submission.rubric.items;
+        var reviews = submission.reviews;
+
+        // Bail early, if there are no reviews - nothing to do
+        if (!reviews.length) {
+          return callback(null, {
+            review: null,
+            satisfied: false
+          });
+        }
+
+        // Sort the reviews by (ascending) date, if required
+        if (reviews.length > 1) {
+          reviews.sort(function (a, b) {
+            if (a.date === b.date)
+              return 0;
+            return a.date < b.date;
+          });
+        }
+
+        var review = reviews.pop();
+        var satisfiedRubrics = review.satisfiedRubrics;
+        var satisfied = false;
+
+        if (satisfiedRubrics.length) {
+          satisfied = true;
+
+          rubrics.forEach(function (rubric, index) {
+            var rubricSatisfied = !rubric.required || (satisfiedRubrics.indexOf(index) >= 0);
+            satisfied &= rubricSatisfied;
+          });
+        }
+
+        callback(null, {
+          review: review,
+          accepted: satisfied
+        });
+      });
+    }
+
+  });
+
+  aestimia.defaultOptions = {
+    auth: {
+      username: 'api',
+      password: SECRET,
+      sendImmediately: false
+    }
+  };
+
+  return aestimia;
+}
